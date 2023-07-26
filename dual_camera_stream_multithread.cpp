f77af2752125526655f42f3bbc30b04bf408d8ca
Digital1O1 <ChristopherTa89@gmail.com>
2023-07-26 17:17:52 -0500

    added atomic stuff

--- dual_camera_stream_multithread.cpp
+++ dual_camera_stream_multithread.cpp
@@ -1,14 +1,25 @@
-#include <opencv2/opencv.hpp>
+#include <opencv2opencv.hpp>
 #include <thread>
 #include <mutex>
-
+#include <stdio.h>
+#include <atomic>
 std::mutex mtx;
 cv::Mat frame1, frame2;
+std::atomic<bool> stopFlag(false);
+
+    int sensor_id = 0;
+    int stread_width = 1280;
+    int stream_height = 720;
+    int frame_rate = 30;
+    int flip_method = 2;
+    int display_width = 960;
+    int display_height = 540;
+
 
 void readVideoFeed1()
 {
     // GStreamer pipeline for IMX219 camera 1
-    std::string pipeline = "nvarguscamerasrc sensor-id=0 ! video/x-raw(memory:NVMM), width=1280, height=720, format=NV12, framerate=30/1 ! nvvidconv flip-method=2 ! video/x-raw, width=960, height=540, format=BGRx ! videoconvert ! video/x-raw, format=BGR ! appsink";
+    std::string pipeline = "nvarguscamerasrc sensor-id=" + std::to_string(sensor_id) + " ! video/x-raw(memory:NVMM), width=" + std::to_string(stread_width) + ", height=" + std::to_string(stream_height) + ", format=NV12, framerate=" + std::to_string(frame_rate) + "/1 ! nvvidconv flip-method=" + std::to_string(flip_method) + " ! video/x-raw, width=" + std::to_string(display_width) + ", height=" + std::to_string(display_height) + ", format=BGRx ! videoconvert ! video/x-raw, format=BGR ! appsink";
 
     cv::VideoCapture cap(pipeline, cv::CAP_GSTREAMER);
 
@@ -18,15 +29,26 @@ void readVideoFeed1()
         return;
     }
 
-    while (true)
+    while (true && !stopFlag.load())
     {
         cv::Mat temp_frame;
         cap >> temp_frame;
+        
         if (temp_frame.empty())
         {
             break;
         }
+       
         mtx.lock();
+       
+
+        if(cv::waitKey(1)=='a')
+        {
+           cv::imwrite("Captured_Image1.jpg",temp_frame);
+           std::cout << "Captured Image 1 " << std::endl;
+           cv::imshow("Captured Image 1",temp_frame);
+        }
+       
         frame1 = temp_frame.clone();
         mtx.unlock();
     }
@@ -34,8 +56,8 @@ void readVideoFeed1()
 
 void readVideoFeed2()
 {
-    // GStreamer pipeline for IMX219 camera 2
-    std::string pipeline = "nvarguscamerasrc sensor-id=1 ! video/x-raw(memory:NVMM), width=1280, height=720, format=NV12, framerate=30/1 ! nvvidconv flip-method=2 ! video/x-raw, width=960, height=540, format=BGRx ! videoconvert ! video/x-raw, format=BGR ! appsink";
+   std::string pipeline = "nvarguscamerasrc sensor-id=" + std::to_string(sensor_id+1) + " ! video/x-raw(memory:NVMM), width=" + std::to_string(stread_width) + ", height=" + std::to_string(stream_height) + ", format=NV12, framerate=" + std::to_string(frame_rate) + "/1 ! nvvidconv flip-method=" + std::to_string(flip_method) + " ! video/x-raw, width=" + std::to_string(display_width) + ", height=" + std::to_string(display_height) + ", format=BGRx ! videoconvert ! video/x-raw, format=BGR ! appsink";
+
     cv::VideoCapture cap(pipeline, cv::CAP_GSTREAMER);
 
     if (!cap.isOpened())
@@ -43,16 +65,25 @@ void readVideoFeed2()
         std::cout << "Error: Unable to open camera 2." << std::endl;
         return;
     }
-
-    while (true)
+    
+    while (true && !stopFlag.load())
     {
         cv::Mat temp_frame;
         cap >> temp_frame;
+   
         if (temp_frame.empty())
         {
             break;
         }
         mtx.lock();
+
+//        if(cv::waitKey(1)=='a')
+//        {
+//           cv::imwrite("Captured_Image1.jpg",temp_frame);
+//           std::cout << "Captured Image 1 " << std::endl;
+//           cv::imshow("Captured Image 1",temp_frame);
+//        }
+
         frame2 = temp_frame.clone();
         mtx.unlock();
     }
@@ -81,18 +112,39 @@ int main()
         }
         mtx.unlock();
 
-        // Check for the 'q' key press to exit the loop
-        if (cv::waitKey(1) == 'q')
+        char key = cv::waitKey(1);
+
+        if(key == 'q')
         {
+            cv::destroyAllWindows();
+            stopFlag.store(true);
+            //exit(0);
             break;
         }
+        if(key == 'a')
+        {
+            cv::imwrite("captured_image1.jpg",frame1);
+            cv::imwrite("captured_image2.jpg",frame2);
+            std::cout <<"Images captured" << std::endl;
+            cv::imshow("Caputred image 1 ",frame1);
+            cv::imshow("Captured image 2 ",frame2);
+        }
+
+        // Check for the 'q' key press to exit the loop
+        //if (cv::waitKey(1) == 'q')
+        //{
+        //    break;
+        //}
+
+
     }
 
     // Wait for both threads to finish
     thread1.join();
     thread2.join();
-
-    cv::destroyAllWindows();
+//    camera.release();
+//    cv::destroyAllWindows();
+    printf("Program complete....\r\n"); 
 
     return 0;
 }
