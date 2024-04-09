e19ca9de88bf8efbf8e81b6ed1ae749a930e6fba
Christopher Ta <Christopher.Ta1125@gmail.com>
2024-04-09 14:37:57 -0500

    Update dual_camera_stream_multithread.cpp

--- dual_camera_stream_multithread.cpp
+++ dual_camera_stream_multithread.cpp
@@ -1,31 +1,29 @@
-#include <opencv2opencv.hpp>
+#include <opencv2/opencv.hpp>
 #include <thread>
 #include <mutex>
-#include <stdio.h>
 #include <atomic>
+
 std::mutex mtx;
 cv::Mat frame1, frame2;
 std::atomic<bool> stopFlag(false);
 
-    int sensor_id = 0;
-    int stread_width = 1280;
-    int stream_height = 720;
-    int frame_rate = 30;
-    int flip_method = 2;
-    int display_width = 960;
-    int display_height = 540;
+int sensor_id = 0;
+int stream_width = 1280;
+int stream_height = 720;
+int frame_rate = 30;
+int flip_method = 2;
+int display_width = 960;
+int display_height = 540;
 
-
-void readVideoFeed1()
+void readVideoFeed(int sensor_id, cv::Mat& frame)
 {
-    // GStreamer pipeline for IMX219 camera 1
-    std::string pipeline = "nvarguscamerasrc sensor-id=" + std::to_string(sensor_id) + " ! video/x-raw(memory:NVMM), width=" + std::to_string(stread_width) + ", height=" + std::to_string(stream_height) + ", format=NV12, framerate=" + std::to_string(frame_rate) + "/1 ! nvvidconv flip-method=" + std::to_string(flip_method) + " ! video/x-raw, width=" + std::to_string(display_width) + ", height=" + std::to_string(display_height) + ", format=BGRx ! videoconvert ! video/x-raw, format=BGR ! appsink";
+    std::string pipeline = "nvarguscamerasrc sensor-id=" + std::to_string(sensor_id) + " ! video/x-raw(memory:NVMM), width=" + std::to_string(stream_width) + ", height=" + std::to_string(stream_height) + ", format=NV12, framerate=" + std::to_string(frame_rate) + "/1 ! nvvidconv flip-method=" + std::to_string(flip_method) + " ! video/x-raw, width=" + std::to_string(display_width) + ", height=" + std::to_string(display_height) + ", format=BGRx ! videoconvert ! video/x-raw, format=BGR ! appsink";
 
     cv::VideoCapture cap(pipeline, cv::CAP_GSTREAMER);
 
     if (!cap.isOpened())
     {
-        std::cout << "Error: Unable to open camera 1." << std::endl;
+        std::cout << "Error: Unable to open camera " << sensor_id << std::endl;
         return;
     }
 
@@ -33,58 +31,14 @@ void readVideoFeed1()
     {
         cv::Mat temp_frame;
         cap >> temp_frame;
-        
+
         if (temp_frame.empty())
         {
             break;
         }
-       
+
         mtx.lock();
-       
-
-        if(cv::waitKey(1)=='a')
-        {
-           cv::imwrite("Captured_Image1.jpg",temp_frame);
-           std::cout << "Captured Image 1 " << std::endl;
-           cv::imshow("Captured Image 1",temp_frame);
-        }
-       
-        frame1 = temp_frame.clone();
-        mtx.unlock();
-    }
-}
-
-void readVideoFeed2()
-{
-   std::string pipeline = "nvarguscamerasrc sensor-id=" + std::to_string(sensor_id+1) + " ! video/x-raw(memory:NVMM), width=" + std::to_string(stread_width) + ", height=" + std::to_string(stream_height) + ", format=NV12, framerate=" + std::to_string(frame_rate) + "/1 ! nvvidconv flip-method=" + std::to_string(flip_method) + " ! video/x-raw, width=" + std::to_string(display_width) + ", height=" + std::to_string(display_height) + ", format=BGRx ! videoconvert ! video/x-raw, format=BGR ! appsink";
-
-    cv::VideoCapture cap(pipeline, cv::CAP_GSTREAMER);
-
-    if (!cap.isOpened())
-    {
-        std::cout << "Error: Unable to open camera 2." << std::endl;
-        return;
-    }
-    
-    while (true && !stopFlag.load())
-    {
-        cv::Mat temp_frame;
-        cap >> temp_frame;
-   
-        if (temp_frame.empty())
-        {
-            break;
-        }
-        mtx.lock();
-
-//        if(cv::waitKey(1)=='a')
-//        {
-//           cv::imwrite("Captured_Image1.jpg",temp_frame);
-//           std::cout << "Captured Image 1 " << std::endl;
-//           cv::imshow("Captured Image 1",temp_frame);
-//        }
-
-        frame2 = temp_frame.clone();
+        frame = temp_frame.clone();
         mtx.unlock();
     }
 }
@@ -95,8 +49,8 @@ int main()
     cv::namedWindow("Two Video Feeds", cv::WINDOW_AUTOSIZE);
 
     // Start two separate threads to read video feeds
-    std::thread thread1(readVideoFeed1);
-    std::thread thread2(readVideoFeed2);
+    std::thread thread1(readVideoFeed, 0, std::ref(frame1));
+    std::thread thread2(readVideoFeed, 1, std::ref(frame2));
 
     while (true)
     {
@@ -114,38 +68,27 @@ int main()
 
         char key = cv::waitKey(1);
 
-        if(key == 'q')
+        if (key == 'q')
         {
             cv::destroyAllWindows();
             stopFlag.store(true);
-            //exit(0);
             break;
         }
-        if(key == 'a')
+        if (key == 'a')
         {
-            cv::imwrite("captured_image1.jpg",frame1);
-            cv::imwrite("captured_image2.jpg",frame2);
-            std::cout <<"Images captured" << std::endl;
-            cv::imshow("Caputred image 1 ",frame1);
-            cv::imshow("Captured image 2 ",frame2);
+            cv::imwrite("captured_image1.jpg", frame1);
+            cv::imwrite("captured_image2.jpg", frame2);
+            std::cout << "Images captured" << std::endl;
+            cv::imshow("Captured image 1", frame1);
+            cv::imshow("Captured image 2", frame2);
         }
-
-        // Check for the 'q' key press to exit the loop
-        //if (cv::waitKey(1) == 'q')
-        //{
-        //    break;
-        //}
-
-
     }
 
     // Wait for both threads to finish
     thread1.join();
     thread2.join();
-//    camera.release();
-//    cv::destroyAllWindows();
-    printf("Program complete....\r\n"); 
+
+    printf("Program complete....\r\n");
 
     return 0;
 }
-
